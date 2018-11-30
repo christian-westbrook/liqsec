f24c860486dc0c42517fcdbfa8c83ebb3f305af1
Christian Westbrook <christianwestbrook@live.com>
2018-11-29 18:30:05 -0600

    Updated index and header

---
+++ public_html/header.php
@@ -0,0 +1,10 @@
+<head>
+    <title>LiqSec</title>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" type="text/css" href="css/header.css" />
+	<link rel="stylesheet" type="text/css" href="css/footer.css" />
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+
+
+</head>
