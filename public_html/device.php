b8d409f5dfa3148401c65c4811e95368ba26af9c
Christian Westbrook <christianwestbrook@live.com>
2018-12-08 05:21:01 -0600

    Updated device and reg-device

---
+++ public_html/device.php
@@ -0,0 +1,23 @@
+<?php
+    // =========================================================================
+    // System     : LiqSec
+    // Repository : https://github.com/christian-westbrook/liqsec.git
+    // File       : device.php
+    // Developers : Nathan Brown, Nicholas Leonard, and Christian
+    // Version    : Pre-release
+    // Abstract   : Displays a report on a particular device
+    // =========================================================================
+
+    $css = array(
+				    0 => ''
+	    	    );
+
+    include 'header.php';
+?>
+
+<div id="container">
+</div>
+
+<?php
+    include 'footer.php';
+?>
---
+++ public_html/reg-device.php
@@ -0,0 +1,23 @@
+<?php
+    // =========================================================================
+    // System     : LiqSec
+    // Repository : https://github.com/christian-westbrook/liqsec.git
+    // File       : reg-device.php
+    // Developers : Nathan Brown, Nicholas Leonard, and Christian
+    // Version    : Pre-release
+    // Abstract   : Registers a new device with LiqSec
+    // =========================================================================
+
+    $css = array(
+				    0 => ''
+	    	    );
+
+    include 'header.php';
+?>
+
+<div id="container">
+</div>
+
+<?php
+    include 'footer.php';
+?>
