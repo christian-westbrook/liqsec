6e1b8544a14e4f0dd02bdd5a79c36e0a737356d4
Christian Westbrook <christianwestbrook@live.com>
2018-12-08 05:37:06 -0600

    Updated device.php

--- public_html/device.php
+++ public_html/device.php
@@ -35,7 +35,15 @@
 			$_POST['ACTIVATE'] = $reuslts[0]['ACTIVATE'];
 			$_POST['OWNER'] = $_SESSION['FNAME'] . ' ' . $_SESSION['LNAME'];
 
-			
+			$sql = 'SELECT * FROM LOGS WHERE DEVICE_ID = :DEV_ID';
+			$stmt = conn->prepare($sql);
+			$stmt->bindParam(":DEV_ID", $_GET['devid'], PDO::PARAM_INT);
+			if($stmt->execute())
+			{
+				$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+				$_POST['LOGS'] = $results;
+			}
 		}
 		else
 		{
@@ -52,9 +60,59 @@
 			header('Location: /~iot3/');
 		}
 	}
+
+	// Time formatter function
+    function formatDateTime($datetime)
+    {
+    	$year = substr($datetime, 0, 4);
+    	$month = substr($datetime, 5, 2);
+    	$day = substr($datetime, 8, 2);
+    	$time = substr($datetime, 11, 8);
+    	switch ($month)
+    	{
+    		case "01": 	$month = 'Jan.';
+    		 			break;
+    		case "02": $month = 'Feb.';
+    					break;
+    		case "03": $month = 'Mar.';
+    					break;
+    		case "04": $month = 'Apr.';
+    					break;
+    		case "05":	$month = 'May';
+    					break;
+    		case "06":	$month = 'June';
+    					break;
+    		case "07": 	$month = 'July';
+    					break;
+    		case "08":	$month = "Aug.";
+    					break;
+    		case "09":	$month = "Sep.";
+    					break;
+    		case "10":	$month = "Oct.";
+    					break;
+    		case "11":	$month = "Nov.";
+    					break;
+    		case "12":	$month = "Dec.";
+    					break;
+    	}
+    	$formatted = $month . ' ' . $day . ', ' . $year . ' ' . $time;
+    	return $formatted;
+    }
 ?>
 
 <div id="container">
+	<?php
+		$devid = $_POST['DEV_ID'];
+		$name =  $_POST['NAME'];
+		$activated = formatDateTime($_POST['ACTIVATE']);
+		$owner = $_POST['OWNER'];
+		$logs = $_POST['LOGS'];
+
+		echo '<p><b>Device ID:</b> ' . $devid . '</br>';
+		echo '<b>Name:</b> ' . $name . '</br>';
+		echo '<b>Owner:</b> ' . $owner . '</br>';
+		echo '<b>Activated:</b> ' . $activated . '</br>';
+	?>
 </div>
 
 <?php
